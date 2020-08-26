a9fba480261ebab60b14e3d7c6aca9d680df5d23
HamsterPunch <msra9my6@gmail.com>
2020-08-26 20:05:24 +0900

    has changed CORS origin in backend

--- backend/app.js
+++ backend/app.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(morgan('dev'));
 }
 app.use(cors({
-    origin : ['http://localhost:4000', 'http://3.34.233.247', 'repeatafter.site'],
+    origin : ['http://localhost:4000', 'http://3.34.233.247', 'http://repeatafter.site'],
     credentials : true
 }));
 app.use('/', express.static(path.join(__dirname, 'upload_image')));
