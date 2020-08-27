83a5b91594d9ca2f1bb8d6f5bd4f6068ae7158ec
HamsterPunch <msra9my6@gmail.com>
2020-08-27 16:37:00 +0900

    Commited for admin signup

--- frontend/components/signup/SignUpForm/index.js
+++ frontend/components/signup/SignUpForm/index.js
@@ -44,7 +44,7 @@ const SignUpForm = () => {
                 email,
                 nickname,
                 password,
-                role: '#user'
+                role: '#user#admin'
             }
         });
     }, [email, nickname, password, passwordCheck, term]);
